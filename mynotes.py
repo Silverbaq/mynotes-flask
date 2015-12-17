1886e16f976292206f6f06e006723d28573f4d06
Steffen Mogensen <feber.sm@gmail.com>
2015-12-17 22:03:47 +0100

    Got something working

--- mynotes.py
+++ mynotes.py
@@ -15,8 +15,6 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
-
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -25,27 +23,62 @@ def connect_db():
 def before_request():
     g.db = connect_db()
 
+
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
-@app.route('/')
-def show_notes():
-    cur = g.db.execute('select Title, Id from Topic order by id desc')
-    topics = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_notes.html', topics = topics)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    cur = g.db.execute('select Id, Title from Topic order by id desc')
+    topics = [dict(id = row[0], title = row[1]) for row in cur.fetchall()]
+    return render_template('select_topic.html', topics=topics)
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_notes'))
+
+
+### TEMP
+@app.route('/test', methods = ['GET', 'POST'])
+def show_notes():
+    cur = g.db.execute('select Id, Title from Topic order by id desc')
+    topics = [dict(id = row[0], title = row[1]) for row in cur.fetchall()]
+
+    subjects = ''
+    notes = ''
+    if request.method == 'POST':
+        topicid = request.form['topic']
+        cur = g.db.execute('select Id, Title from Subject where TopicId = (?)', topicid)
+        subjects = [dict(id = row[0], title = row[1]) for row in cur.fetchall()]
+
+        if 'subject' in request.form:
+            subjectId = request.form['subject']
+            cur = g.db.execute('select Id, Value, Title from Note where SubjectId = (?)', subjectId)
+            notes = [dict(id = row[0], value = row[1], title = row[2]) for row in cur.fetchall()]
+
+    return render_template('show_notes.html', topics = topics, subjects = subjects, notes = notes)
+
+
+@app.route('/selected_topic', methods=['POST'])
+def selected_topic():
+    topicid = [request.args.get('topicid')]
+    cur = g.db.execute('select Id, Title from Subject where (?)', topicid)
+    subjects = [dict(id = row[0], title = row[1]) for row in cur.fetchall()]
+    return render_template('select_subject.html', subjects = subjects)
+
+
+@app.route('/addTopic', methods=['GET','POST'])
+def add_topic():
+    try:
+        top = request.form['topic']
+        g.db.execute('insert into Topic (Title) values (?)',
+                     [top])
+        g.db.commit()
+        flash('New topic was successfully added')
+    except Exception as e:
+        flash(e.message)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
